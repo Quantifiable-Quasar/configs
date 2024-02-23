df73ffe3322a61a9e606e6812bcb352701c72408
Quantifiable-Quasar <itsmeamario.tti@gmail.com>
2024-02-23 14:23:20 -0500

    initilization script

--- init.sh
+++ init.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-wd=$(pwd)
 
 mv .bashrc ~/.bashrc
 mv .vimrc ~/.vimrc
@@ -11,4 +10,9 @@ mkdir -p ~/.config/awesome
 mv rc.lua ~/.config/awesome/
 mv screenshot.lua ~/.config/awesome/
 
-rm -rf $wd
+read -p "Are you in the git dir? [y/N]" yn 
+if [ $yn == 'y' ] && [ $(pwd | rev | cut -d "/" -f 1) == 'sgifnoc' ]
+then
+        rm -rf $(pwd)
+fi
+
